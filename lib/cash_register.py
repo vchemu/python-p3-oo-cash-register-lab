cd671f028ff726d04180218f0bb3a729d8e1f48f
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-29 16:37:59 +0300

    castRegister

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.existing_transactions = []
+
+  def add_item(self, title, price, quantity=1):
+    self.total += price * quantity
+    for _ in range(quantity):
+      self.items.append(title)
+    self.existing_transactions.append({"title": title, "price": price, "quantity": quantity})
+
+  def apply_discount(self):
+    if self.discount > 0:
+      self.total = int(self.total * ((100 - self.discount) / 100)) #self.discount / 100
+      print(f"After the discount, the total comes to ${self.total}.")
+    else:
+      print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    if not self.existing_transactions:
+      return "There are no transactions to void."
+    self.total -= (self.existing_transactions[-1]["price"] * self.existing_transactions[-1]["quantity"]
+    )
+    for _ in range(self.existing_transactions[-1]["quantity"]):
+      self.items.pop()
+    self.existing_transactions.pop()
+
